545ed966e52437e4e2c5453da0ad2ef498b66999
Christopher Huayhua <chuanca@cochabamba.bo>
2024-09-04 16:27:32 -0400

    Architecture of project

--- config/configDatabase.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(`${process.env.DATABASE_CONNECTION}://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_SERVER}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`)
